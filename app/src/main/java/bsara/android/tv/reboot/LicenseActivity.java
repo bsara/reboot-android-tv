3808b802974e9d5a496f74027f98aa175498490c
Brandon Sara <bsara.dev@gmail.com>
2015-01-19 21:50:38 -0700

    updated launcher icon and added some play store assets

--- app/src/main/java/bsara/android/tv/reboot/LicenseActivity.java
+++ app/src/main/java/bsara/android/tv/reboot/LicenseActivity.java
@@ -3,8 +3,39 @@ package bsara.android.tv.reboot;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 
 public class LicenseActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+
+    this.setContentView(R.layout.activity_license);
+
+
+    final String licenseURL = this.getString(R.string.activity_license_url);
+
+
+    WebView webView = (WebView)this.findViewById(R.id.license_webView);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public void onLoadResource(WebView view, String url) {
+        if (!url.equals(licenseURL)) {
+          LicenseActivity.this.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+          return;
+        }
+        super.onLoadResource(view, url);
+      }
+    });
+    webView.loadUrl(this.getString(R.string.activity_license_url));
+  }
 }
